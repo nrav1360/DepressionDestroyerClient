32a396587f942e349b52dd35f049402347913af8
nrav1360 <68169720+nrav1360@users.noreply.github.com>
2020-07-12 21:19:11 -0700

    nearly done

--- InsultSimulator.java
+++ InsultSimulator.java
@@ -2,6 +2,7 @@ package aarav.programs;
 
 import java.awt.Color;
 import java.awt.Font;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.text.SimpleAttributeSet;
@@ -11,8 +12,13 @@ import javax.swing.text.StyledDocument;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class InsultSimulator implements ActionListener{
 	
@@ -25,17 +31,30 @@ public class InsultSimulator implements ActionListener{
 	private boolean special;
 	private boolean yomamaON, oldtimeyON, rareON, cringeON;
 	private String set = "\n\n";
+	private static  String[] inputs = new String[3];
+	private static String[] gInsults = new String[7];
+	private static String[] yInsults = new String[7];
+	private static String[] oInsults = new String[7];
+	private static String[] rInsults = new String[7];
+	private static String[] cInsults = new String[7];
+	
 	
 	public InsultSimulator() throws IOException{
+		
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 500, 700);
 		frame.setTitle("Insult Simulator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		panel = new JPanel();
-		panel.setBackground(Color.blue);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
+		
+	//	BufferedImage ate = ImageIO.read(new File("aarav/programs/prettyforest.png"));
+	//	JLabel cre = new JLabel(new ImageIcon(ate));
+	//	cre.setBounds(0, 0, 500, 300);
+	//	panel.add(cre); 
 		
 		insult = new JGradientButton("Insult");
 		insult.setBounds(25, 300, 200, 50);
@@ -64,25 +83,29 @@ public class InsultSimulator implements ActionListener{
 		
 		yo = new JLabel();
 		yo.setBounds(95, 395, 100, 20);
-		yo.setFont(new Font("Papyrus", Font.PLAIN, 14));
+		yo.setForeground(Color.white);
+		yo.setFont(new Font("Papyrus", Font.BOLD, 14));
 		yo.setText("Yo Mama");
 		panel.add(yo);
 		
 		yo = new JLabel();
 		yo.setBounds(95, 525, 100, 20);
-		yo.setFont(new Font("Papyrus", Font.PLAIN, 14));
+		yo.setForeground(Color.white);
+		yo.setFont(new Font("Papyrus", Font.BOLD, 14));
 		yo.setText("Old Timey");
 		panel.add(yo);
 		
 		yo = new JLabel();
 		yo.setBounds(355, 395, 100, 20);
-		yo.setFont(new Font("Papyrus", Font.PLAIN, 14));
+		yo.setForeground(Color.white);
+		yo.setFont(new Font("Papyrus", Font.BOLD, 14));
 		yo.setText("Rare");
 		panel.add(yo);
 		
 		yo = new JLabel();
 		yo.setBounds(352, 525, 100, 20);
-		yo.setFont(new Font("Papyrus", Font.PLAIN, 14));
+		yo.setForeground(Color.white);
+		yo.setFont(new Font("Papyrus", Font.BOLD, 14));
 		yo.setText("Cringe");
 		panel.add(yo);
 
@@ -123,6 +146,19 @@ public class InsultSimulator implements ActionListener{
 		JLabel bubble = new JLabel(new ImageIcon(speech));
 		bubble.setBounds(150, -40, 150, 230);
 		panel.add(bubble); 
+		
+		BufferedImage ate = ImageIO.read(new File("aarav/programs/prettyforest.png"));
+		JLabel cre = new JLabel(new ImageIcon(ate));
+		cre.setBounds(0, 0, 500, 282);
+		panel.add(cre); 
+		
+		BufferedImage ke = ImageIO.read(new File("aarav/programs/flowriver.png"));
+		JLabel ma = new JLabel(new ImageIcon(ke));
+		ma.setBounds(0, 282, 500, 418);
+		panel.add(ma); 
+		
+		
+		
 		
 		frame.setVisible(true); 
 		
@@ -252,8 +288,96 @@ public class InsultSimulator implements ActionListener{
 			}
 		}
 	}
+	
+	
+	
+	public static String splice(String string) {
+		
+		int begIndex = string.indexOf(':');
+		int endIndex = string.indexOf(',');
+		String toBeReturned = string.substring(begIndex+2, endIndex-1);
+		return toBeReturned.substring(1);
+		
+	}
+	
+	public static int randomize(){
+		return (int) (Math.random() * (6 - 0 + 1) + 1);
+	}
 
 	public static void main(String[] args) throws IOException {
+		/*URL url = new URL("http://localhost:3000/insult/list");
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader( con.getInputStream()));
+		String st; 
+		String combined = "";
+		  int value = 0;
+		 while ((st = in.readLine()) != null) {
+		    System.out.println(st); 
+		    combined = combined + st;
+		 } 
+		
+		File file = new File("aarav/programs/Routes.txt"); 
+		  
+		 BufferedReader br = new BufferedReader(new FileReader(file)); 
+		  
+		String st; 
+		String combined = "";
+		  int value = 0;
+		 while ((st = br.readLine()) != null) {
+		    System.out.println(st); 
+		    combined = combined + st;
+			
+		 }   
+		 int bracketCounter = 0;
+		 for(int i=0; i<combined.length(); i++){
+			 if(combined.charAt(i)=='{' || combined.charAt(i)=='}')
+			 bracketCounter++;
+		 }
+		 
+		 for(int i=0; i<bracketCounter/2;i++){
+			 int beg = combined.indexOf('{');
+			 int end = combined.indexOf('}');
+			 String car = combined.substring(beg, end+1);
+			 inputs[value] = car;
+			 combined = combined.substring(end+1);
+			 value++;
+		 }
+		
+		int yCounter = 0;
+		int oCounter = 0;
+		int rCounter = 0;
+		int cCounter = 0;
+		int gCounter = 0;
+		System.out.println(inputs[0]);
+		for(int i = 0; i<inputs.length; i++) {
+			if(inputs[i].contains("yo mama")) {
+				yInsults[yCounter] = inputs[i];
+				yCounter++;
+			}
+			else if(inputs[i].contains("[\"old timey\"]")) {
+				oInsults[oCounter] = inputs[i];
+				oCounter++;
+				System.out.println("hi");
+			}
+			else if(inputs[i].contains("rare")) {
+				rInsults[rCounter] = inputs[i];
+				rCounter++;
+			}
+			else if(inputs[i].contains("cringe")) {
+				cInsults[cCounter] = inputs[i];
+				cCounter++;
+			}
+			else {
+				gInsults[gCounter] = inputs[i];
+				gCounter++;
+			}
+		}
+		System.out.println(oInsults[0]);
+		System.out.println(oInsults[1]);
+		System.out.println(oInsults[2]);
+		System.out.print(splice(oInsults[0])); */
 		new InsultSimulator();
 	}
 	
